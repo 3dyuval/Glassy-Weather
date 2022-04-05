4a36cd276efc9edafcbd19a75082ca477f3beb7f
3dyuval <3dyuval@gmail.com>
2022-04-05 22:49:51 +0300

    css

--- src/Components/Main/Background.animation.jsx
+++ src/Components/Main/Background.animation.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { animated as a, useTransition } from "react-spring"
-import "./SCSS/Background.Animation.scss"
 
 function BackgroundAnimation({ animate, graphic }) {
 
     const [show, setShow] = useState(false)
 
     const transition = useTransition(show, {
-        from: { y: 1000, opacity: 0 },
-        enter: { y: -250, opacity: 1 },
-        leave: { y: 1000, opacity: 0 },
+        from: { transform: "translateY(500px)", opacity: 0 },
+        enter: { transform: "translateY(0px)", opacity: 1 },
+        leave: { transform: "translateY(500px)", opacity: 0 },
         reset: true,
         onRest: () => setShow(true),
         delay: 500,
