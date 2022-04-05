1c9cf2f611b4ddbe6c344b27ed0813014d159cfb
3dyuval <3dyuval@gmail.com>
2022-04-05 22:28:49 +0300

    updated

--- api/index.js
+++ api/index.js
@@ -13,17 +13,18 @@ app.listen(PORT, () => {
 })
 
 app.get('/city/:city', (req, res) => {
-
     const options = {
         method: "GET",
         url: `http://api.weatherapi.com/v1/forecast.json?key=${process.env.VITE_API_KEY}&q=${req.params.city}&days=3&aqi=no&alerts=no`
     }
-
     axios.request(options)
         .then(response => {
             return new Promise((resolve, reject) => {
                 if (response.status === 200) resolve(res.json(response.data))
                 if (response.status !== 200) reject(`response status: ${response.status}`)
             })
+        }).catch(error => {
+            return error
         })
+
 })
