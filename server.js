f3a341d97404859795a4282c49ee7363bfcdecd0
3dyuval <3dyuval@gmail.com>
2022-04-06 10:38:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.listen(port, () => {
 
 // serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('dist'))
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-    })
+    app.use(express.static(path.join(__dirname, 'dist')))
 }
 
 app.get('/city/:city', (req, res) => {
