6b311b20fa13e00b12abbb2290ef43966505f958
3dyuval <3dyuval@gmail.com>
2022-04-06 18:32:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { viteExternalsPlugin as externals } from 'vite-plugin-externals';
 
 export default defineConfig({
-  ...,
   esbuild: {
     jsxFactory: "React.createElemennt",
     jsxFragment: "React.Fragment"
