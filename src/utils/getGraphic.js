aa88aced055b1299244f6d08d281565e1b2727a6
3dyuval <3dyuval@gmail.com>
2022-04-08 18:42:32 +0300

    graphics

--- src/utils/getGraphic.js
+++ src/utils/getGraphic.js
@@ -3,5 +3,5 @@ import codes from "/src/Assets/WeatherConditions"
 export function getGraphic(code) {
     const svg = codes.find(itm => itm.code == code)
     if (!svg) return ''
-    return `/src/Assets/WeatherConditions/${svg.icon}.svg`
+    return `./assets/WeatherConditions/${svg.icon}.svg`
 }
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from '@honkhonk/vite-plugin-svgr'
-
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-        ]
-      }
-    }),
-    svgr()
-  ]
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build') {
+    return {
+      plugins: [
+        react({
+          babel: {
+            plugins: [
+              ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+            ]
+          }
+        }),
+        svgr(),
+        copy({
+          targets: [{ src: 'src/Assets/WeatherConditions', dest: 'dist/assets' }],
+          hook: 'writeBundle'
+        }),
+      ]
+    }
+  } else {
+    return {
+      server: {
+        port: 3001
+      },
+      plugins: [
+        react(),
+        svgr()
+      ]
+    }
+  }
 })
+
+
+
+
