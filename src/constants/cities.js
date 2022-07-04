2155956dc1b4c1c8a80647be406f9d7f5833efbc
Yuval Dikerman <3dyuval@gmail.com>
2022-07-04 11:49:48 +0300

    Update cities.js

--- src/constants/cities.js
+++ src/constants/cities.js
@@ -1,8 +1,9 @@
-export const DEFAULT_CITIES = [{ name: "Tel Aviv", id: 0, selected: true }]
-
-export const CITIES_EXAMPLES = [
+export const DEFAULT_CITIES = [
     { name: "Tel Aviv", id: 0, selected: true },
     { name: "Tokyo", id: 1 },
     { name: "New-York", id: 2 },
-    { name: "Rome", id: 3 }
+    { name: "Rome", id: 3 },
+    { name: "Beijing", id: 4 },
+    { name: "Moscow", id: 5 },
+
 ]
