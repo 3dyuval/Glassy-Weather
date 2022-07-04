922fdd829ad1a933beb6d227a988203464f34f44
Yuval Dikerman <3dyuval@gmail.com>
2022-07-04 14:08:26 +0300

    Update colors.jsx

--- src/utils/colors.jsx
+++ src/utils/colors.jsx
@@ -68,10 +68,10 @@ const colors = {
 
 
 colors.colorTypes = {
-    light1: { hex: "#EFF7FF", name: "light1", url: `assets/colors/light.css` },
-    light2: { hex: "#D5DCEC", name: "light2", url: `assets/colors/light.css` },
-    dark1: { hex: "#6B84AE", name: "dark1", url: `assets/colors/dark.css` },
-    dark2: { hex: "#384153", name: "dark2", url: `assets/colors/dark.css` }
+    light1: { hex: "#EFF7FF", name: "light1", url: `/assets/colors/light.css` },
+    light2: { hex: "#D5DCEC", name: "light2", url: `/assets/colors/light.css` },
+    dark1: { hex: "#6B84AE", name: "dark1", url: `/assets/colors/dark.css` },
+    dark2: { hex: "#384153", name: "dark2", url: `/assets/colors/dark.css` }
 }
 
 
